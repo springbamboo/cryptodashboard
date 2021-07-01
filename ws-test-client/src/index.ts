72aecc4b17de770306a295cd9802566f6e62d409
大井さかな <41501869+01397@users.noreply.github.com>
2021-07-02 01:15:29 +0900

    受け取りメッセージの表示

--- ws-test-client/src/index.ts
+++ ws-test-client/src/index.ts
@@ -1,50 +1,80 @@
 let socket: WebSocket = null
 
+type OutputStyle = 'raw' | 'json'
+
 document.addEventListener('DOMContentLoaded', () => {
   const urlField = document.getElementById('url') as HTMLInputElement
   const dataField = document.getElementById('data-textbox') as HTMLInputElement
+  const styleSelector = document.getElementById('output-style') as HTMLSelectElement
+  const outputInfoBox = document.getElementById('output-info') as HTMLDivElement
+  const outputBodyBox = document.getElementById('output-body') as HTMLDivElement
+  const connectButton = document.getElementById('connect-button') as HTMLButtonElement
+  const disconnectButton = document.getElementById('disconnect-button') as HTMLButtonElement
+  const sendButton = document.getElementById('send-button') as HTMLButtonElement
+  let mode: OutputStyle = 'raw'
 
-  document.getElementById('connect-button').addEventListener('click', () => {
+  connectButton.addEventListener('click', () => {
     const url = urlField.value
     connect(url)
   })
 
-  document.getElementById('disconnect-button').addEventListener('click', () => {
+  disconnectButton.addEventListener('click', () => {
     socket.close()
   })
 
-  document.getElementById('send-button').addEventListener('click', () => {
+  sendButton.addEventListener('click', () => {
     const data = dataField.value
     send(data)
   })
+
+  styleSelector.addEventListener('change', () => {
+    mode = styleSelector.value as OutputStyle
+  })
+
+  function connect(url: string) {
+    socket = new WebSocket(url)
+
+    socket.addEventListener('open', (e) => {
+      console.log('open')
+      output('open')
+    })
+
+    socket.addEventListener('message', (e) => {
+      console.log(`message: ${new Date().toISOString()}\n${e.data}`)
+      output('message', e.data)
+    })
+
+    socket.addEventListener('close', (e) => {
+      console.log('close')
+      console.log(e)
+      output('close')
+    })
+
+    socket.addEventListener('error', (e) => {
+      console.log('error')
+      console.log(e)
+      output('error')
+    })
+  }
+
+  function send(data: string) {
+    socket.send(data)
+  }
+
+  function output(type: string, message: string = '') {
+    let info = new Date().toISOString() + ' ' + type
+    let body = ''
+    if (mode === 'raw') {
+      body += message
+    } else if (mode === 'json') {
+      try {
+        body += JSON.stringify(JSON.parse(message), null, 4)
+      } catch (e) {
+        info += ' (JSON parsing failed)'
+        body += message
+      }
+    }
+    outputInfoBox.innerText = info
+    outputBodyBox.innerText = body
+  }
 })
-
-
-
-function connect(url: string) {
-  socket = new WebSocket(url)
-
-  socket.addEventListener('open', (e) => {
-    console.log('open')
-    console.log(e)
-  })
-
-  socket.addEventListener('message', (e) => {
-    console.log('message')
-    console.log(e)
-  })
-
-  socket.addEventListener('close', (e) => {
-    console.log('close')
-    console.log(e)
-  })
-
-  socket.addEventListener('error', (e) => {
-    console.log('error')
-    console.log(e)
-  })
-}
-
-function send(data: string) {
-  socket.send(data)
-}
