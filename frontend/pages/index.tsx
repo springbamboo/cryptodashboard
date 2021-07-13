fa449ee477c2db67d0b2c2e8662d18d8c71228a4
大井さかな <41501869+01397@users.noreply.github.com>
2021-07-13 23:27:03 +0900

    タイトル変更

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -45,7 +45,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Crypto currency platform!!
         </h1>
 
         <p className={styles.description}>
