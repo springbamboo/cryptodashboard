bd0e1168db52c75d8b836b7e1a8d5cc61e325cac
springbamboo <wuchengzhong1125@gmail.com>
2021-08-14 13:49:59 +0900

    add ws file

---
+++ frontend/services/front_back_socket.ts
@@ -0,0 +1,28 @@
+import {useState, useEffect} from 'react';
+
+const WS_URL = 'ws://localhost:5001'
+
+export default function useWsService(setPrice) {
+  const [socket, _] = useState(() => (typeof WebSocket !== 'undefined' ? new WebSocket(WS_URL) : null))
+  useEffect(() => {
+    if (typeof WebSocket !== 'undefined') {
+      socket.addEventListener('open', (e) => {
+        console.log('open')
+      })
+      socket.addEventListener('message', (e) => {
+        setPrice(JSON.parse(e.data).BTCUSDT_p)
+      })
+      socket.addEventListener('close', (e) => {
+        console.log('close')
+        console.log(e)
+      })
+      socket.addEventListener('error', (e) => {
+        console.log('error')
+        console.log(e)
+      })
+    }
+  },[])
+  // return wsdata;
+}
+
+// export default useWsService
