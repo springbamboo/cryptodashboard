71ed56e6cdce52b0cf1033cf882413f6ed3f86f7
springbamboo <wuchengzhong1125@gmail.com>
2021-08-29 00:52:06 +0900

    static candlestick from btcusdt@binance

--- backend/detail/binanceD.ts
+++ backend/detail/binanceD.ts
@@ -4,23 +4,30 @@ import http from "http";
 
 const ws: WebSocket = new WebSocket(" wss://fstream.binance.com/ws");
 const httpEndPoint: string = "https://fapi.binance.com";
-const httpKline: string = "/fapi/v1/klines?symbol=btcusdt&interval=5m&limit=100";
+const httpKline: string =
+    "/fapi/v1/klines?symbol=btcusdt&interval=1m&limit=3";
 
-const app = http.createServer((req, res) => {
-        res.writeHead(200, { "Content-Type": "text/plain" });
+const app = http
+    .createServer((req, res) => {
+        res.writeHead(200, { 
+            "Content-Type": "text/plain",
+            'Access-Control-Allow-Origin': '*',
+        });
         const promise = axios.get(httpEndPoint + httpKline);
         promise.then((response) => {
             const newData = response.data;
             const cdata = newData.map((d: string[]) => {
-                const date = new Date(d[0])
+                const date = new Date(d[0]);
                 return {
-                    "open": date,
-                    "high": parseFloat(d[1]),
-                    "low": parseFloat(d[2]),
-                    "close":parseFloat(d[3]),
-                    "quantity":parseFloat(d[4])
+                    time: date,
+                    open: parseFloat(d[1]),
+                    high: parseFloat(d[2]),
+                    low: parseFloat(d[3]),
+                    close: parseFloat(d[4]),
+                    quantity: parseFloat(d[5]),
                 };
             });
             res.end(JSON.stringify(cdata));
-        })
-    }).listen(5000);
+        });
+    })
+    .listen(5000);
--- frontend/components/CandleStick.tsx
+++ frontend/components/CandleStick.tsx
@@ -1,70 +1,46 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Chart from "react-google-charts";
+import React from "react";
+import dynamic from "next/dynamic";
+const ReactApexChart = dynamic(() => import("react-apexcharts"), {
+    ssr: false,
+});
 
-const data = [
-    ["time", "low", "open", "close", "high"],
-    ["1", 20, 28, 38, 45],
-    ["2", 31, 38, 55, 66],
-    ["3", 50, 55, 77, 80],
-    ["4", 77, 77, 66, 50],
-    ["5", 68, 66, 22, 15],
-    ["1", 20, 28, 38, 45],
-    ["2", 31, 38, 55, 66],
-    ["3", 50, 55, 77, 80],
-    ["4", 77, 77, 66, 50],
-    ["5", 68, 66, 22, 15],
-    ["1", 20, 28, 38, 45],
-    ["2", 31, 38, 55, 66],
-    ["3", 50, 55, 77, 80],
-    ["4", 77, 77, 66, 50],
-    ["5", 68, 66, 22, 15],
-    ["1", 20, 28, 38, 45],
-    ["2", 31, 38, 55, 66],
-    ["3", 50, 55, 77, 80],
-    ["4", 77, 77, 66, 50],
-    ["5", 68, 66, 22, 15],
-    ["1", 20, 28, 38, 45],
-    ["2", 31, 38, 55, 66],
-    ["3", 50, 55, 77, 80],
-    ["4", 77, 77, 66, 50],
-    ["5", 68, 66, 22, 15],
-    ["1", 20, 28, 38, 45],
-    ["2", 31, 38, 55, 66],
-    ["3", 50, 55, 77, 80],
-    ["4", 77, 77, 66, 50],
-    ["5", 68, 66, 22, 15],
-]
+const CandleStick = () => {
+    const series = [{
+        data: [
+        ]
+    }
+    ];
+    const options = {
+        chart: {
+            type: "candlestick",
+            height: 350,
+            background:'black'
+        },
+        title: {
+            text: "CandleStick Chart",
+            align: "left",
+        },
+        xaxis: {
+            type: "datetime",
+        },
+        yaxis: {
+            tooltip: {
+                enabled: false,
+            },
+        },
+    };
 
-const option = {
-    legend: "none",
-    backgroundColor:"grey",
-    candlestick: {
-        fallingColor: { strokeWidth: 0, fill: 'red' }, 
-        risingColor: { strokeWidth: 0, fill: 'green' }, 
-      },
-};
-
-const CandleDisplay = () => {
-    // useEffect(() => {
-    //     axios
-    //     .get("http://localhost:5000")
-    //     .then(response => {
-    //         console.log(response.data);
-    //     })
-    // },[])
     return (
-        <div>
-        <Chart
-            width={"100%"}
-            height={350}
-            chartType="CandlestickChart"
-            data={data}
-            options={option}
-            rootProps={{ "data-testid": "1" }}
-        />
+        <div id="chart">
+            <ReactApexChart
+                //  @ts-ignore
+                options={options}
+                series={series}
+                type="candlestick"
+                height={350}
+            />
         </div>
     );
 };
 
-export default CandleDisplay;
+export default CandleStick;
