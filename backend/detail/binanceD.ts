6bed9b0c9d30f1e937c924f74da0c1ae92e14513
springbamboo <wuchengzhong1125@gmail.com>
2021-08-29 22:15:00 +0900

    candlestick @binance finish

--- backend/detail/binanceD.ts
+++ backend/detail/binanceD.ts
@@ -1,33 +1,43 @@
 import WebSocket from "ws";
 import axios from "axios";
-import http from "http";
+import express from "express";
+
+const app = express();
 
 const ws: WebSocket = new WebSocket(" wss://fstream.binance.com/ws");
 const httpEndPoint: string = "https://fapi.binance.com";
-const httpKline: string =
-    "/fapi/v1/klines?symbol=btcusdt&interval=1m&limit=3";
+const httpKline: string = "/fapi/v1/klines?symbol=btcusdt&interval=1m&limit=50";
 
-const app = http
-    .createServer((req, res) => {
-        res.writeHead(200, { 
-            "Content-Type": "text/plain",
-            'Access-Control-Allow-Origin': '*',
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+app.get("/binance/btcusdt", (req, res) => {
+    const promise = axios.get(httpEndPoint + httpKline);
+    promise.then((response: { data: any; }) => {
+        const newData = response.data;
+        const cdata = newData.map((d: string[]) => {
+            const date = new Date(d[0]);
+            // return 
+            // {
+            //     // time: date,
+            //     // open: parseFloat(d[1]),
+            //     // high: parseFloat(d[2]),
+            //     // low: parseFloat(d[3]),
+            //     // close: parseFloat(d[4]),
+            //     // quantity: parseFloat(d[5]),
+            // };
+            return [
+                d[0],
+                parseFloat(d[1]),
+                parseFloat(d[2]),
+                parseFloat(d[3]),
+                parseFloat(d[4]),
+                // parseFloat(d[5]),
+            ]
         });
-        const promise = axios.get(httpEndPoint + httpKline);
-        promise.then((response) => {
-            const newData = response.data;
-            const cdata = newData.map((d: string[]) => {
-                const date = new Date(d[0]);
-                return {
-                    time: date,
-                    open: parseFloat(d[1]),
-                    high: parseFloat(d[2]),
-                    low: parseFloat(d[3]),
-                    close: parseFloat(d[4]),
-                    quantity: parseFloat(d[5]),
-                };
-            });
-            res.end(JSON.stringify(cdata));
-        });
-    })
-    .listen(5000);
+        res.end(JSON.stringify(cdata));
+    });
+});
+app.listen(5000);
--- frontend/components/CandleStick.tsx
+++ frontend/components/CandleStick.tsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
+import axios from "axios";
 const ReactApexChart = dynamic(() => import("react-apexcharts"), {
     ssr: false,
 });
 
 const CandleStick = () => {
-    const series = [{
-        data: [
-        ]
-    }
+    const [klineDdata, setKlineData] = useState([]);
+    const series = [
+        {
+            data: klineDdata,
+        },
     ];
+    // console.log(series[0].data);
+    useEffect(() => {
+        const promise = axios("http://localhost:5000/binance/btcusdt");
+        promise.then((res) => {
+            setKlineData(res.data);
+            console.log(res.data[49][4]);
+        }).catch(err => console.log(err));
+    },[]);
+
     const options = {
         chart: {
             type: "candlestick",
             height: 350,
-            background:'black'
+            background: "black",
         },
         title: {
             text: "CandleStick Chart",
