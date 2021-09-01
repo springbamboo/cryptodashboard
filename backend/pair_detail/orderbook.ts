141122a9e41839897af66bb37b6dbcf349357e4a
springbamboo <wuchengzhong1125@gmail.com>
2021-09-01 22:43:26 +0900

    set format

--- backend/pair_detail/orderbook.ts
+++ backend/pair_detail/orderbook.ts
@@ -13,44 +13,80 @@ app.get(`/binance/:pair`, (req, res) => {
     const promise = axios.get(httpEndPoint + httpOrderBook);
     promise.then((response) => {
         const newData = response.data;
-        const bidOne:number = req.params.pair === 'xrpusdt' ? Math.round(parseFloat(newData.bids[0][0]) * 1000) / 1000:parseInt(newData.bids[0][0]);
-        const bidLast:number = req.params.pair === 'xrpusdt' ? Math.round(parseFloat(newData.bids[limit - 1][0]) * 1000) / 1000:parseInt(newData.bids[limit - 1][0]);
-        const askOne:number = req.params.pair === 'xrpusdt' ? Math.round(parseFloat(newData.asks[0][0]) * 1000) / 1000:parseInt(newData.asks[0][0]);
-        const askLast:number = req.params.pair === 'xrpusdt' ? Math.round(parseFloat(newData.asks[limit - 1][0]) * 1000) / 1000:parseInt(newData.asks[limit - 1][0]);
-        const delta: number = req.params.pair === 'xrpusdt' ? 0.001 : 1;
-        const widthBid:number = req.params.pair === 'xrpusdt' ? Math.round((bidOne - bidLast + delta) *  1000): bidOne - bidLast + delta;
+        const bidOne: number =
+            req.params.pair === "xrpusdt"
+                ? Math.round(parseFloat(newData.bids[0][0]) * 1000) / 1000
+                : parseInt(newData.bids[0][0]);
+        const bidLast: number =
+            req.params.pair === "xrpusdt"
+                ? Math.round(parseFloat(newData.bids[limit - 1][0]) * 1000) /
+                  1000
+                : parseInt(newData.bids[limit - 1][0]);
+        const askOne: number =
+            req.params.pair === "xrpusdt"
+                ? Math.round(parseFloat(newData.asks[0][0]) * 1000) / 1000
+                : parseInt(newData.asks[0][0]);
+        const askLast: number =
+            req.params.pair === "xrpusdt"
+                ? Math.round(parseFloat(newData.asks[limit - 1][0]) * 1000) /
+                  1000
+                : parseInt(newData.asks[limit - 1][0]);
+        const delta: number = req.params.pair === "xrpusdt" ? 0.001 : 1;
+        const widthBid: number =
+            req.params.pair === "xrpusdt"
+                ? Math.round((bidOne - bidLast + delta) * 1000)
+                : bidOne - bidLast + delta;
         const xbid: number[] = [];
         const ybid: number[] = new Array(widthBid).fill(0);
         const xask: number[] = [];
-        const widthAsk:number = req.params.pair === 'xrpusdt' ? Math.round((askLast - askOne + delta) *  1000): askLast - askOne + delta;
+        const widthAsk: number =
+            req.params.pair === "xrpusdt"
+                ? Math.round((askLast - askOne + delta) * 1000)
+                : askLast - askOne + delta;
         const yask: number[] = new Array(widthAsk).fill(0);
         let index_bid = 0;
-        for (let i = bidOne; i >= bidLast; i-= delta) {
+        for (let i = bidOne; i >= bidLast; i -= delta) {
             xbid.push(i);
-            for(let j = 0; j < limit; j++){
-                if(parseFloat(newData.bids[j][0]) < i + delta && parseFloat(newData.bids[j][0]) >= i){
+            for (let j = 0; j < limit; j++) {
+                if (
+                    parseFloat(newData.bids[j][0]) < i + delta &&
+                    parseFloat(newData.bids[j][0]) >= i
+                ) {
                     ybid[index_bid] += parseFloat(newData.bids[j][1]);
                 }
             }
             index_bid += 1;
         }
         let index_ask = 0;
-        for(let i = askOne; i <= askLast; i+=delta){
+        for (let i = askOne; i <= askLast; i += delta) {
             xask.push(i);
-            for(let j = 0; j < limit; j++){
-                if(parseFloat(newData.asks[j][0]) >= i && parseFloat(newData.asks[j][0]) < i + delta){
+            for (let j = 0; j < limit; j++) {
+                if (
+                    parseFloat(newData.asks[j][0]) >= i &&
+                    parseFloat(newData.asks[j][0]) < i + delta
+                ) {
                     yask[index_ask] += parseFloat(newData.asks[j][1]);
                 }
             }
             index_ask += 1;
         }
-        for(let i = 0; i < yask.length; i++){
-            xask[i] = req.params.pair === 'xrpusdt' ? parseFloat((Math.floor(xask[i] * 1000) / 1000).toFixed(3)) : Math.floor(xask[i]);
-            yask[i] = parseFloat((Math.floor(yask[i] * 1000) / 1000).toFixed(3));
+        for (let i = 0; i < yask.length; i++) {
+            xask[i] =
+                req.params.pair === "xrpusdt"
+                    ? parseFloat((Math.floor(xask[i] * 1000) / 1000).toFixed(3))
+                    : Math.floor(xask[i]);
+            yask[i] = parseFloat(
+                (Math.floor(yask[i] * 1000) / 1000).toFixed(3)
+            );
         }
-        for(let j = 0; j < ybid.length; j++){
-            xbid[j] = req.params.pair === 'xrpusdt' ? parseFloat((Math.floor(xbid[j] * 1000) / 1000).toFixed(3)) : Math.floor(xbid[j]);
-            ybid[j] = parseFloat((Math.floor(ybid[j] * 1000) / 1000).toFixed(3));
+        for (let j = 0; j < ybid.length; j++) {
+            xbid[j] =
+                req.params.pair === "xrpusdt"
+                    ? parseFloat((Math.floor(xbid[j] * 1000) / 1000).toFixed(3))
+                    : Math.floor(xbid[j]);
+            ybid[j] = parseFloat(
+                (Math.floor(ybid[j] * 1000) / 1000).toFixed(3)
+            );
         }
         const orderbook = [xask, yask, xbid, ybid];
         res.end(JSON.stringify(orderbook));
