efe0f5e97820ed34050ff2b350e03eb714190aea
springbamboo <wuchengzhong1125@gmail.com>
2021-09-03 01:46:45 +0900

    change pair

--- backend/pair_detail/candlestick_bybit.ts
+++ backend/pair_detail/candlestick_bybit.ts
@@ -4,16 +4,15 @@ import express from "express";
 
 const app = express();
 
-const ws: WebSocket = new WebSocket("wss://stream.bybit.com/spot/quote/ws/v2");
-const httpEndPoint: string = "https://api.bybit.com";
-const httpKline: string = "/spot/quote/v1/kline?symbol=BTCUSDT&interval=1m&limit=200";
-
 app.all("*", function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     next();
 })
 
-app.get("/bybit/btcusdt", (req, res) => {
+app.get("/bybit/:pair", (req, res) => {
+    const pairName = req.params.pair;
+    const httpEndPoint: string = "https://api.bybit.com";
+    const httpKline: string = `/spot/quote/v1/kline?symbol=${pairName}&interval=1m&limit=200`;
     const promise = axios.get(httpEndPoint + httpKline);
     promise.then((response: { data: any}) => {
          const newData = response.data.result;
@@ -29,5 +28,4 @@ app.get("/bybit/btcusdt", (req, res) => {
          res.end(JSON.stringify(cdata));
   });
 });
-
 app.listen(5000);
