233ec30df544d84b33af19505c930949dada25f2
大井さかな <41501869+01397@users.noreply.github.com>
2021-09-02 22:37:45 +0900

    WS初期接続時の表示ラグを解消

--- backend/exchange/controll.ts
+++ backend/exchange/controll.ts
@@ -10,7 +10,23 @@ const server = new WebSocket.Server({ port: 5001, path: "/home" });
 
 let clients: WebSocket[] = [];
 
+// クライアント初期接続時に送るために一時保管
+const tempPairs: { [coin: string]: { [key: string]: Coindata } | null } = {
+    binance: null,
+    bybit: null,
+    bitfinex: null,
+    huobi: null,
+    okex: null,
+};
+
 server.on("connection", (ws: WebSocket) => {
+    // 初期値を送信
+    for (const key in tempPairs) {
+        if (!tempPairs.hasOwnProperty(key)) break;
+        if (tempPairs[key] === null) break;
+        ws.send(JSON.stringify(tempPairs[key]));
+    }
+    // 接続を配列に追加
     clients.push(ws);
     console.log(clients.length);
 });
@@ -25,6 +41,10 @@ const eventEmitterList = [
 
 for (const eventEmitter of eventEmitterList) {
     eventEmitter.on("change", (pairs: { [key: string]: Coindata }) => {
+        // 一時保管データの更新
+        const exchangeName: string = pairs["BTCUSDT"].exchange;
+        tempPairs[exchangeName] = pairs;
+        // クライアントへ送信
         for (let i = 0; i < clients.length; i++) {
             clients[i].send(JSON.stringify(pairs));
         }
